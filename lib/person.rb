8bf22539ba3531c5b3cf4f09cb929f7e712ec537
munenebrian <njerubrianmunene@gmail.com>
2022-08-19 17:45:30 +0300

    done!

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
-
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def job=(job)
+    @job = job
+  end
+  def job
+    @job
+  end
 end
+Kinuthia = Person.new
+Kinuthia.name = "Kinuthia"
+Kinuthia.job = "SoftWare Developer"
